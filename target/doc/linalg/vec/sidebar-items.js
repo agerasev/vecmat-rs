ea68f73b50d5ead5f1b41b3e000ff053eb48b3b9
Alexey Gerasev <alexey.gerasev@gmail.com>
2017-05-18 13:44:39 +0700

    update docs

--- target/doc/linalg/vec/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["Vec2",""],["Vec3",""],["Vec4",""]],"type":[["Vec2bool",""],["Vec2f32",""],["Vec2f64",""],["Vec2i32",""],["Vec2u32",""],["Vec3bool",""],["Vec3f32",""],["Vec3f64",""],["Vec3i32",""],["Vec3u32",""],["Vec4bool",""],["Vec4f32",""],["Vec4f64",""],["Vec4i32",""],["Vec4u32",""]]});
---
+++ target/doc/vecmat/vec/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Vec2",""],["Vec3",""],["Vec4",""]],"trait":[["Dot",""]],"type":[["Vec2bool",""],["Vec2f32",""],["Vec2f64",""],["Vec2i32",""],["Vec2u32",""],["Vec3bool",""],["Vec3f32",""],["Vec3f64",""],["Vec3i32",""],["Vec3u32",""],["Vec4bool",""],["Vec4f32",""],["Vec4f64",""],["Vec4i32",""],["Vec4u32",""]]});
