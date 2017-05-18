d55e04552d7e86aa8e33a38e2e290fcb23144168
Alexey Gerasev <alexey.gerasev@gmail.com>
2017-05-19 02:02:54 +0700

    update docs

--- target/doc/implementors/core/iter/iterator/trait.Iterator.js
+++ target/doc/implementors/core/iter/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["num_integer"] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html' title='core::iter::iterator::Iterator'>Iterator</a> for <a class='struct' href='num_integer/struct.IterBinomial.html' title='num_integer::IterBinomial'>IterBinomial</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='num_integer/trait.Integer.html' title='num_integer::Integer'>Integer</a> + <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a></span>",];
+implementors["num_integer"] = ["impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"num_integer/struct.IterBinomial.html\" title=\"struct num_integer::IterBinomial\">IterBinomial</a>&lt;T&gt; <span class=\"where fmt-newline\">where T: <a class=\"trait\" href=\"num_integer/trait.Integer.html\" title=\"trait num_integer::Integer\">Integer</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
