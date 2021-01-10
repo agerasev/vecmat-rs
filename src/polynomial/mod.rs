7390e1cf278cf409c892d05f07648ddfc220d5bd
Alexey Gerasev <alexey.gerasev@gmail.com>
2021-01-10 10:43:09 +0700

    Ugly workaround to get the polynomial of order N + 1

--- src/polynomial/mod.rs
+++ src/polynomial/mod.rs
@@ -5,170 +5,127 @@ use core::{
         AddAssign, SubAssign, MulAssign, DivAssign, RemAssign,
     },
     cmp::{max, min},
+    iter,
 };
 use num_traits::{Num, Float, Zero, One};
 use num_complex::Complex;
 use crate::{Vector, traits::IntoComplex};
 
 
-/// Polynomial up to order of `N - 1`.
+/// Polynomial up to degree of `N`.
 /// 
 /// The coefficients are stored reverese manner, e.g.
 /// 
 /// ```
 /// a[0] + a[1]*x + a[2]*x^2 + a[3]*x^3 + ...
 /// ```
-#[repr(transparent)]
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Polynomial<T, const N: usize> {
+    // Ugly workaround because we cannot write `Vector<T, N + 1>` for now.
+    constant: T,
     coeffs: Vector<T, N>,
 }
 
 impl<T, const N: usize> Polynomial<T, N> {
-    pub fn from_array(array: [T; N]) -> Self {
-        Self { coeffs: array.into() }
+    pub fn from_coeffs(constant: T, coeffs: Vector<T, N>) -> Self {
+        Self { constant, coeffs }
     }
-    pub fn into_array(self) -> [T; N] {
-        self.coeffs.into_array()
+    pub fn into_coeffs(self) -> (T, Vector<T, N>) {
+        (self.constant, self.coeffs)
     }
-    pub fn as_array(&self) -> &[T; N] {
-        self.coeffs.as_array()
+    pub fn as_coeffs(&self) -> (&T, &Vector<T, N>) {
+        (&self.constant, &self.coeffs)
     }
-    pub fn as_mut_array(&mut self) -> &mut [T; N] {
-        self.coeffs.as_mut_array()
-    }
-    pub fn from_vector(coeffs: Vector<T, N>) -> Self {
-        Self { coeffs }
-    }
-    pub fn into_vector(self) -> Vector<T, N> {
-        self.coeffs
-    }
-    pub fn as_vector(&self) -> &Vector<T, N> {
-        &self.coeffs
-    }
-    pub fn as_mut_vector(&mut self) -> &mut Vector<T, N> {
-        &mut self.coeffs
-    }
-}
-
-impl<T, const N: usize> From<[T; N]> for Polynomial<T, N> {
-    fn from(coeffs: [T; N]) -> Self {
-        Self::from_array(coeffs)
-    }
-}
-impl<T, const N: usize> Into<[T; N]> for Polynomial<T, N> {
-    fn into(self) -> [T; N] {
-        self.into_array()
-    }
-}
-impl<T, const N: usize> AsRef<[T; N]> for Polynomial<T, N> {
-    fn as_ref(&self) -> &[T; N] {
-        self.as_array()
-    }
-}
-impl<T, const N: usize> AsMut<[T; N]> for Polynomial<T, N> {
-    fn as_mut(&mut self) -> &mut [T; N] {
-        self.as_mut_array()
-    }
-}
-
-impl<T, const N: usize> From<Vector<T, N>> for Polynomial<T, N> {
-    fn from(coeffs: Vector<T, N>) -> Self {
-        Self::from_vector(coeffs)
-    }
-}
-impl<T, const N: usize> Into<Vector<T, N>> for Polynomial<T, N> {
-    fn into(self) -> Vector<T, N> {
-        self.into_vector()
-    }
-}
-impl<T, const N: usize> AsRef<Vector<T, N>> for Polynomial<T, N> {
-    fn as_ref(&self) -> &Vector<T, N> {
-        self.as_vector()
-    }
-}
-impl<T, const N: usize> AsMut<Vector<T, N>> for Polynomial<T, N> {
-    fn as_mut(&mut self) -> &mut Vector<T, N> {
-        self.as_mut_vector()
+    pub fn as_mut_coeffs(&mut self) -> (&mut T, &mut Vector<T, N>) {
+        (&mut self.constant, &mut self.coeffs)
     }
 }
 
 impl<T, const N: usize> Index<usize> for Polynomial<T, N> {
     type Output = T;
     fn index(&self, idx: usize) -> &T {
-        &self.coeffs[idx]
+        match idx {
+            0 => &self.constant,
+            _ => &self.coeffs[idx + 1],
+        }
     }
 }
 impl<T, const N: usize> IndexMut<usize> for Polynomial<T, N> {
     fn index_mut(&mut self, idx: usize) -> &mut T {
-        &mut self.coeffs[idx]
+        match idx {
+            0 => &mut self.constant,
+            _ => &mut self.coeffs[idx + 1],
+        }
     }
 }
 impl<T, const N: usize> IntoIterator for Polynomial<T, N> {
     type Item = T;
-    type IntoIter = <Vector<T, N> as IntoIterator>::IntoIter;
+    type IntoIter = iter::Chain<iter::Once<T>, <Vector<T, N> as IntoIterator>::IntoIter>;
     fn into_iter(self) -> Self::IntoIter {
-        self.coeffs.into_iter()
+        iter::once(self.constant).chain(self.coeffs.into_iter())
     }
 }
 impl<T, const N: usize> Polynomial<T, N> {
     fn iter(&self) -> impl Iterator<Item=&T> {
-        self.coeffs.iter()
+        iter::once(&self.constant).chain(self.coeffs.iter())
     }
     fn iter_mut(&mut self) -> impl Iterator<Item=&mut T> {
-        self.coeffs.iter_mut()
+        iter::once(&mut self.constant).chain(self.coeffs.iter_mut())
     }
 }
 
 impl<T: Zero, const N: usize> Zero for Polynomial<T, N> {
     fn zero() -> Self {
-        Self::from(Vector::zero())
+        Self::from_coeffs(T::zero(), Vector::zero())
     }
     fn is_zero(&self) -> bool {
-        self.coeffs.iter().all(|x| x.is_zero())
+        self.constant.is_zero() && self.coeffs.iter().all(|x| x.is_zero())
     }
 }
 impl<T: Default, const N: usize> Default for Polynomial<T, N> {
     fn default() -> Self {
-        Self::from(Vector::default())
+        Self::from_coeffs(T::default(), Vector::default())
     }
 }
 
 impl<T: Neg<Output=T>, const N: usize> Neg for Polynomial<T, N> {
     type Output = Self;
     fn neg(self) -> Self {
-        Self::from(-self.coeffs)
+        Self::from_coeffs(-self.constant, -self.coeffs)
     }
 }
 impl<T: Add<Output=T>, const N: usize> Add for Polynomial<T, N> {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Self::from(self.coeffs + other.coeffs)
+        Self::from_coeffs(self.constant + other.constant, self.coeffs + other.coeffs)
     }
 }
 impl<T: Sub<Output=T>, const N: usize> Sub for Polynomial<T, N> {
     type Output = Self;
     fn sub(self, other: Self) -> Self {
-        Self::from(self.coeffs - other.coeffs)
+        Self::from_coeffs(self.constant - other.constant, self.coeffs - other.coeffs)
     }
 }
 impl<T: AddAssign, const N: usize> AddAssign for Polynomial<T, N> {
     fn add_assign(&mut self, other: Self) {
+        self.constant += other.constant;
         self.coeffs += other.coeffs;
     }
 }
 impl<T: SubAssign, const N: usize> SubAssign for Polynomial<T, N> {
     fn sub_assign(&mut self, other: Self) {
+        self.constant -= other.constant;
         self.coeffs -= other.coeffs;
     }
 }
 
 impl<T: Zero, const N: usize> Polynomial<T, N> {
+    /// The degree of the polynomial **plus one**.
     pub fn order(&self) -> usize {
         let mut o = 0;
-        for (i, c) in self.coeffs.iter().enumerate().rev() {
+        for (i, c) in self.iter().enumerate().rev() {
             if !c.is_zero() {
-                o = i;
+                o = i + 1;
                 break;
             }
         }
